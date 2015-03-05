436803f986e0eebdd7344f5a57e0f93ad21e889b
Tang Rufus <tangrufus@gmail.com>
2015-03-05 17:57:59 +0800

    Devise: Add admin with manual approval

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,51 @@
 # == Route Map
 #
-#                   Prefix Verb   URI Pattern                       Controller#Action
-#         new_user_session GET    /users/sign_in(.:format)          devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)          devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)         devise/sessions#destroy
-#            user_password POST   /users/password(.:format)         devise/passwords#create
-#        new_user_password GET    /users/password/new(.:format)     devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
-#                          PATCH  /users/password(.:format)         devise/passwords#update
-#                          PUT    /users/password(.:format)         devise/passwords#update
-# cancel_user_registration GET    /users/cancel(.:format)           devise/registrations#cancel
-#        user_registration POST   /users(.:format)                  devise/registrations#create
-#    new_user_registration GET    /users/sign_up(.:format)          devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)             devise/registrations#edit
-#                          PATCH  /users(.:format)                  devise/registrations#update
-#                          PUT    /users(.:format)                  devise/registrations#update
-#                          DELETE /users(.:format)                  devise/registrations#destroy
-#        user_confirmation POST   /users/confirmation(.:format)     devise/confirmations#create
-#    new_user_confirmation GET    /users/confirmation/new(.:format) devise/confirmations#new
-#                          GET    /users/confirmation(.:format)     devise/confirmations#show
-#              user_unlock POST   /users/unlock(.:format)           devise/unlocks#create
-#          new_user_unlock GET    /users/unlock/new(.:format)       devise/unlocks#new
-#                          GET    /users/unlock(.:format)           devise/unlocks#show
-#                     home GET    /home(.:format)                   redirect(301, /)
-#                     root GET    /                                 high_voltage/pages#show {:id=>"home"}
-#                     page GET    /*id                              high_voltage/pages#show
+#                    Prefix Verb   URI Pattern                       Controller#Action
+#         new_admin_session GET    /admins/sign_in(.:format)         devise/sessions#new
+#             admin_session POST   /admins/sign_in(.:format)         devise/sessions#create
+#     destroy_admin_session DELETE /admins/sign_out(.:format)        devise/sessions#destroy
+#            admin_password POST   /admins/password(.:format)        devise/passwords#create
+#        new_admin_password GET    /admins/password/new(.:format)    devise/passwords#new
+#       edit_admin_password GET    /admins/password/edit(.:format)   devise/passwords#edit
+#                           PATCH  /admins/password(.:format)        devise/passwords#update
+#                           PUT    /admins/password(.:format)        devise/passwords#update
+# cancel_admin_registration GET    /admins/cancel(.:format)          devise/registrations#cancel
+#        admin_registration POST   /admins(.:format)                 devise/registrations#create
+#    new_admin_registration GET    /admins/sign_up(.:format)         devise/registrations#new
+#   edit_admin_registration GET    /admins/edit(.:format)            devise/registrations#edit
+#                           PATCH  /admins(.:format)                 devise/registrations#update
+#                           PUT    /admins(.:format)                 devise/registrations#update
+#                           DELETE /admins(.:format)                 devise/registrations#destroy
+#              admin_unlock POST   /admins/unlock(.:format)          devise/unlocks#create
+#          new_admin_unlock GET    /admins/unlock/new(.:format)      devise/unlocks#new
+#                           GET    /admins/unlock(.:format)          devise/unlocks#show
+#          new_user_session GET    /users/sign_in(.:format)          devise/sessions#new
+#              user_session POST   /users/sign_in(.:format)          devise/sessions#create
+#      destroy_user_session DELETE /users/sign_out(.:format)         devise/sessions#destroy
+#             user_password POST   /users/password(.:format)         devise/passwords#create
+#         new_user_password GET    /users/password/new(.:format)     devise/passwords#new
+#        edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
+#                           PATCH  /users/password(.:format)         devise/passwords#update
+#                           PUT    /users/password(.:format)         devise/passwords#update
+#  cancel_user_registration GET    /users/cancel(.:format)           devise/registrations#cancel
+#         user_registration POST   /users(.:format)                  devise/registrations#create
+#     new_user_registration GET    /users/sign_up(.:format)          devise/registrations#new
+#    edit_user_registration GET    /users/edit(.:format)             devise/registrations#edit
+#                           PATCH  /users(.:format)                  devise/registrations#update
+#                           PUT    /users(.:format)                  devise/registrations#update
+#                           DELETE /users(.:format)                  devise/registrations#destroy
+#         user_confirmation POST   /users/confirmation(.:format)     devise/confirmations#create
+#     new_user_confirmation GET    /users/confirmation/new(.:format) devise/confirmations#new
+#                           GET    /users/confirmation(.:format)     devise/confirmations#show
+#               user_unlock POST   /users/unlock(.:format)           devise/unlocks#create
+#           new_user_unlock GET    /users/unlock/new(.:format)       devise/unlocks#new
+#                           GET    /users/unlock(.:format)           devise/unlocks#show
+#                      home GET    /home(.:format)                   redirect(301, /)
+#                      root GET    /                                 high_voltage/pages#show {:id=>"home"}
+#                      page GET    /*id                              high_voltage/pages#show
 #
 
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
 end
