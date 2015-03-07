6a71c3624752d5e098a0595b65ab308bae92a8a3
Tang Rufus <tangrufus@gmail.com>
2015-03-07 09:54:24 +0800

    Reports: Add products and bills

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,5 +5,9 @@ class ReportsController < ApplicationController
     @order_total_commission_charge = Order.total_commission_charge current_company
 
     @earnings = @order_total_amount - @order_total_commission_charge - @bill_total_amount
+
+    @bills = Bill.where(company: current_company)
+    @orders = Order.where(company: current_company)
+    @products = Product.where(company: current_company)
   end
 end
