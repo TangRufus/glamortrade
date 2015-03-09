7808a08ae8acbde91222598f2752bfe6b46769fb
Tang Rufus <tangrufus@gmail.com>
2015-03-09 10:55:07 +0800

    Production: HTTPS

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -136,7 +136,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # config.rememberable_options = {}
+  config.rememberable_options = {:secure => Rails.env.production?}
 
   # ==> Configuration for :validatable
   # Range for password length.
@@ -209,11 +209,11 @@ Devise.setup do |config|
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
-  # config.default_scope = :user
+  config.default_scope = :user
 
   # Set this configuration to false if you want /users/sign_out to sign out
   # only the current scope. By default, Devise signs out all scopes.
-  # config.sign_out_all_scopes = true
+  config.sign_out_all_scopes = true
 
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
