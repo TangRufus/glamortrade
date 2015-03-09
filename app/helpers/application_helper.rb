7bce247770d35c50317b5ea82f89f0613b7ff2f5
Tang Rufus <tangrufus@gmail.com>
2015-03-09 11:37:06 +0800

    View: Add &yen;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_company
     current_user.company if current_user.present?
   end
+
+  def number_to_yen number, options = {}
+    number_to_currency number, unit: "&yen;"
+  end
 end
